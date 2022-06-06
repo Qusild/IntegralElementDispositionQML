1793a1240e1c6686f80516d24e550293af68487a
Qusild <74545363+Qusild@users.noreply.github.com>
2022-06-06 09:52:57 +0300

    Update back.h

--- back/back.h
+++ back/back.h
@@ -2,6 +2,7 @@
 #include <array>
 #include <unordered_map>
 #include <vector>
+#include <QObject>
 
 struct coordinates
 {
@@ -19,13 +20,13 @@ struct connection
 {
 public:
     connection(int ini, int fin, int id)
-        : initial(ini), final(fin), id(id) {}
+        : initial(ini), final(fin), id(id){}
     connection(int ini, int fin, int id, std::string name)
         : initial(ini), final(fin), id(id), name(name) {}
 
     int initial, final;
-    std::string name;
     int id;
+    std::string name;
 
 public:
     bool operator==(const connection& other) { return this->id == other.id; }
@@ -43,8 +44,8 @@ public:
         : coords({ x, y }), id(id), name(n) {}
 
     coordinates coords;
-    std::string name;
     int id;
+    std::string name;
     std::vector<connection> connections;
 
 
@@ -56,9 +57,25 @@ public:
     void remove_connection(connection& conn);
 };
 
-class Schema
+class Schema : public QObject
 {
+    Q_OBJECT
 public:
+    Schema(const Schema &other):QObject(other.parent())
+    {
+        dimentions_x= other.dimentions_x;
+        dimentions_y= other.dimentions_y;
+        elements = other.elements;
+        schema_map = other.schema_map;
+    }
+    Schema operator=(const Schema &other)
+    {
+        dimentions_x= other.dimentions_x;
+        dimentions_y= other.dimentions_y;
+        elements = other.elements;
+        schema_map = other.schema_map;
+        return *this;
+    }
     Schema(int x, int y)
         : dimentions_x(x), dimentions_y(y) {
         for (int i = 0; i < dimentions_y; i++)
@@ -71,27 +88,48 @@ public:
     };
 
     int dimentions_x, dimentions_y;
-    double scale;
     std::vector<integral_element> elements;
     std::vector<std::vector<int>> schema_map;
 
     void add_element(const integral_element& new_element);
     void remove_element(integral_element& element);
 
-
     integral_element* find(int id);
-
+    connection* find_con(int id);
+    int scale;
     void clear_map();
+    int setID(int type);
+public slots:
+    void addConnection(int init,int fin);
+    void addElement(QString ElemName, qint32 ini);
+    void delElement(int ini);
+    void startUpdate();
+    void updateGrid();
+    void readFile(QString filename);
+    void writeFile(QString filename);
+    void delConnection(int init, int fin);
+signals:
+    void sendCell(int id, int pos, int type, QString name, QString con_1, QString con_2, QString con_3, QString con_4);
+    void sendCon(int pos, QString con);
+    void updModel(int dim_x, int dim_y);
 };
 
-class Back
+class Back : public QObject
 {
+    Q_OBJECT
 public:
-    Schema read_file(std::string filename);
-    int write_file(Schema schema, std::string filename);
+    static Schema read_file(std::string filename, Schema& schema);
+    static void write_file(Schema& schema, std::string filename);
 
     static Schema genetic_update(Schema&);
     static int A_star(Schema&, connection);
+
+
+public slots:
+    void decypher(std::string filename);
+
+signals:
+    void sendProps();
 };
 
 int is_invalid(Schema* schema, coordinates place);
